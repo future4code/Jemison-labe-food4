4324bfa69073149700e40f9820c340eb54b78558
jose neto <jnetocdz@hotmail.com.br>
2022-09-28 20:38:33 -0300

    pagina de login que ainda não renderiza no navegador

--- labe-food4/src/pages/loginpage/index.js
+++ labe-food4/src/pages/loginpage/index.js
@@ -1,7 +1,60 @@
+import styled from "styled-components"
+import { useForm } from "../../hook"
+import {LoginPageContainer, FormContainer} from './styled'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Button,
+    Input,
+} from '@chakra-ui/react'
+import { useState } from "react"
+
 export const LoginPage = () => {
 
+    const [form, onChangeInputs, clearInputs] = useForm({
+        email: "",
+        password: ""
+    })
+
+    const [isEmailValid, setIsEmailValid] = useState(true)
+    const [showPassword, setShowpassword] = useState(false)
+
+    const onSubmit=(e)=>{
+        e.preventDefault()
+        console.log(form);
+        setIsEmailValid(/[a-zA-Z0-9]@[a-z]{3}\{.a-z}?/.test(form.email))
+    }
+
+    const onClickShowPassword=()=> {
+        setShowpassword(!showPassword)
+    }
+
     return (
-        <h1>Login Page</h1>
+        <LoginPageContainer>
+            <h1>dadadasdsada</h1>
+            <FormContainer>
+                <form onSubmit={onSubmit}>
+                    <FormControl isInvalid={!isEmailValid}>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            name='email'
+                            type='email'
+                            value={form.email}
+                            onChange={onChangeInputs}
+                            placeholder='email'
+                        />
+                        {!isEmailValid ? (
+                            <FormHelperText>
+                            E-mail inválido..
+                            </FormHelperText>
+                        ) : undefined}
+                    </FormControl>
+                    <Button type='submit' variant='form'>Enviar</Button>
+                </form>
+            </FormContainer>
+        </LoginPageContainer>
     )
 
 
---
+++ labe-food4/src/pages/loginpage/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+
+export const LoginPageContainer = styled.div`
+
+    height: 500px;
+    width: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+`
+
+export const FormContainer = styled.div`
+    width: 40vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: red;
+`
